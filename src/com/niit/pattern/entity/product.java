517de0ab56968d7240d77d4d915d53941d86d296
pavanpoojari <pavan.poojary035@gmail.com>
2016-11-24 02:02:43 -0800

    This is my first commit

---
+++ src/com/niit/pattern/entity/product.java
@@ -0,0 +1,74 @@
+package com.niit.pattern.entity;
+
+public class product {
+	
+	
+	public product(int id, int quantity, String name, String brand, String category, String description, double price) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.name = name;
+		this.brand = brand;
+		this.category = category;
+		this.description = description;
+		this.price = price;
+	}	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "product [id=" + id + ", quantity=" + quantity + ", name=" + name + ", brand=" + brand + ", category="
+				+ category + ", description=" + description + ", price=" + price + "]";
+	}
+
+	private int  id;
+	private int  quantity;
+	private String name;
+	private String brand;
+	private String category;
+	private String description;
+	private double price ;
+	
+}
